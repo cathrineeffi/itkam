4f947380b18f60033cfeaf7005dcd0ac77d17225
cathrineffi <cathrine.efimova@gmail.com>
2019-09-18 17:23:58 +0800

    12 les

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://otvet.imgsmail.ru/download/263951837_910c5e9d089a3e6439947862d496f607_800.png" width="100"/>
+        </header>
+    )
+}
+
+export default Header;
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="nav">
+            <div>
+                <a href="">Profile</a>
+                <a href="">Messages</a>
+                <a href="">News</a>
+                <a href="">Music</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
+
